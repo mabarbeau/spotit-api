119dd227f7adbb8af9b20312f568e8d94cdb4655
Marc Barbeau <marcbarbeau@MARCB343.local>
2019-09-04 10:32:23 -0600

    Restrict CORS to allowed origins

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,8 +8,15 @@ class Cors
 {
   public function handle($request, Closure $next)
   {
+    $allowedOrigins = ['localhost', 'http://spotit.local:8080'];
+
+    $origin = $request->server('HTTP_ORIGIN');
+
+    if (in_array($origin, $allowedOrigins)) {
       return $next($request)
-          ->header('Access-Control-Allow-Origin', '*')
-          ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+        ->header('Access-Control-Allow-Origin', $origin)
+        ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    }
+    return $next($request);
   }
 }

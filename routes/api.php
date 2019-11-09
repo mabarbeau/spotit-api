b9281664314a742293813c0a531f8fea593c0e19
Marc Barbeau <mabarbeau1@gmail.com>
2019-11-09 09:16:34 -0800

    Delete routes

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,3 @@ Route::get('users/{id}', 'UserController@show');
 Route::resource('spots', 'SpotController')->except([
     'create', 'edit'
 ]);
-
-
-Route::get('Snapshots/{alias}/{key}', 'SnapshotController@index');
-Route::post('Snapshots', 'SnapshotController@restore');

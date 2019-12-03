2c322fc3ce3a61bf8c3740d378cfd8c0aa4812f5
Marc Barbeau <mabarbeau1@gmail.com>
2019-12-03 07:57:02 -0700

    Combined seeders

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Map;
+use App\Spot;
+use App\User;
+use App\Feature;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,26 +15,30 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(App\User::class, 50)->create()->each([$this, 'eachUser']);
+        factory(User::class, 50)->create()->each([$this, 'eachUser']);
     }
     
     function eachUser($user)
     {
+        $isPostmanSpotCreated = Spot::where(['slug' => 'postman'])->exists();
+        if (!$isPostmanSpotCreated) {
+            factory(Spot::class, 1)->create(['creator_id' => $user->id, 'slug' => 'postman'])->each([$this, 'eachSpot']);
+        }
         if (rand(1, 10) < 8) {
-            $user->spots()->saveMany(factory(App\Spot::class, rand(1, 5))->make())->each([$this, 'eachSpot']);
+            $user->spots()->saveMany(factory(Spot::class, rand(1, 5))->make())->each([$this, 'eachSpot']);
         }
     }
 
     function eachSpot($spot)
     {
-        $spot->map()->save(factory(App\Map::class)->make());
+        $spot->map()->save(factory(Map::class)->make());
         if (rand(1, 10) < 8) {
-            $spot->features()->saveMany(factory(App\Feature::class, rand(1, 5))->make())->each([$this, 'eachFeature']);
+            $spot->features()->saveMany(factory(Feature::class, rand(1, 5))->make())->each([$this, 'eachFeature']);
         }
     }
 
     function eachFeature($feature)
     {
-        $feature->map()->save(factory(App\Map::class)->make());
+        $feature->map()->save(factory(Map::class)->make());
     }
 }

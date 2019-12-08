b3e34825882a8fe00691eb7361e374d4a0638be4
Marc Barbeau <mabarbeau1@gmail.com>
2019-12-08 13:42:39 -0700

    404 response

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,7 +3,9 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -46,6 +48,15 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
+        if (
+            $exception instanceof ModelNotFoundException
+            || $exception instanceof NotFoundHttpException
+        ) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Page not found'
+            ], 404);
+        }
         return parent::render($request, $exception);
     }
 }

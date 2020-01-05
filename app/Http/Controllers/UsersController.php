47fb571ccfc79cfe20d69879b3e8e963bc027f51
Marc Barbeau <mabarbeau1@gmail.com>
2020-01-05 12:00:31 -0800

    Unhide id

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
      */
     public function index(Request $request)
     {
-        return User::select('name')->paginate();
+        return User::select('id', 'name')->paginate();
     }
 
     /**
